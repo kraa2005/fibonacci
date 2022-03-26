83fb3a6257ec96b7e3607bb2ac4893d5021cd233
kraa <karurahashimoto@gmail.com>
2022-03-26 15:44:32 -0700

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
 'use strict';
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
 function fib(n) {
-    if (n === 0){
-        return 0;
-    } else if (n === 1){
-        return 1;
-    }
-return fib(n - 1) + fib(n - 2);
+  if (memo.has(n)) {
+    return memo.get(n);
+  }
+  const value = fib(n - 1) + fib(n - 2);
+  memo.set(n, value);
+  return value;
 }
-console.log(`${10}番目のfibonacci数は${fib(10)}です`)
-//
-
-const length = 10;
-for(let i = 0; i <= length; i++){
-    console.log(fib(i))
+const length = 40;
+for (let i = 0; i <= length; i++) {
+  console.log(fib(i));
 }
 
-//when adding this{} into the String, you need to start and end with ` this thing. shift+ key below esc
-
-// console.log(`${process.argv[2]}番目のfibonacci数は${fib(process.argv[2])}です`)
-
-//const length = process.argv[2];
-//for(let i = 0; i <= length; i++){
-    //console.log(fib(i))
-//}
-//console.log(`${process.argv[2]}番目のfibonacci数は${fib(process.argv[2])}です`)
