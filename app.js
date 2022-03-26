1b068d7839d1e5160fd4bf7119fbc41bcc99a642
kraa <karurahashimoto@gmail.com>
2022-03-26 15:02:09 -0700

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -10,15 +10,15 @@ return fib(n - 1) + fib(n - 2);
 console.log(`${10}番目のfibonacci数は${fib(10)}です`)
 //
 
-const length = 40;
+const length = 10;
 for(let i = 0; i <= length; i++){
     console.log(fib(i))
 }
 
 //when adding this{} into the String, you need to start and end with ` this thing. shift+ key below esc
-return 
-console.log(`${process.argv[2]}番目のfibonacci数は${fib(process.argv[2])}です`)
-return 
+
+// console.log(`${process.argv[2]}番目のfibonacci数は${fib(process.argv[2])}です`)
+
 //const length = process.argv[2];
 //for(let i = 0; i <= length; i++){
     //console.log(fib(i))
